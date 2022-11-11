a6466aa0cc77d0629c568a3912e98bfc4d81a56a
Asutosh  Chamappaji Urs <asutoshurs@gmail.com>
2022-11-11 21:23:19 +0530

    m

---
+++ t_converter.py
@@ -0,0 +1,22 @@
+num =33
+
+print(bin(num))
+print(oct(num))
+print(hex(num))
+print(float(num))
+
+i = int('100001',2)
+j = int('41',8)
+k = int('21',16)
+
+print(i,j,k)
+
+#hex_val = "DC 14 3C"
+#hex_val = int(hex_val,16)
+#print(hex_val)
+
+r = int('DC',16)
+g = int('14',16)
+b = int('3C',16)
+
+print(r,g,b)
